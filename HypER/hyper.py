6572c4c0eb2c513056ab38f1680b6e00e38c0377
Chief <luyolo.nqobile@gmail.com>
2020-01-07 07:01:28 +0200

    Log Train-Valid-Test Metrics
    Refactor Hyper class and add requirements.

--- HypER/hyper.py
+++ HypER/hyper.py
@@ -1,22 +1,41 @@
-from load_data import Data
+# std
+import time
+from collections import defaultdict
+import argparse
+import pickle
+
+# 3rd party
 import numpy as np
 import torch
 from torch.nn import functional as F, Parameter
 from torch.nn.init import xavier_normal_, xavier_uniform_
-import time
-from collections import defaultdict
-from models import *
 from torch.optim.lr_scheduler import ExponentialLR
-import argparse
-import pickle
+
+# internal
+from load_data import Data
+from models import *
 
     
 class Experiment:
 
-    def __init__(self, model_name, learning_rate=0.001, ent_vec_dim=200, rel_vec_dim=200, 
-                 num_iterations=100, batch_size=128, decay_rate=0., cuda=False, 
-                 input_dropout=0., hidden_dropout=0., feature_map_dropout=0.,
-                 in_channels=1, out_channels=32, filt_h=3, filt_w=3, label_smoothing=0.):
+    def __init__(self,
+                 model_name,
+                 learning_rate=0.001,
+                 ent_vec_dim=200,
+                 rel_vec_dim=200,
+                 num_iterations=100,
+                 batch_size=128,
+                 decay_rate=0.,
+                 cuda=False,
+                 input_dropout=0.,
+                 hidden_dropout=0.,
+                 feature_map_dropout=0.,
+                 in_channels=1,
+                 out_channels=32,
+                 filt_h=3,
+                 filt_w=3,
+                 label_smoothing=0.):
+
         self.model_name = model_name
         self.learning_rate = learning_rate
         self.ent_vec_dim = ent_vec_dim
@@ -26,35 +45,47 @@ class Experiment:
         self.decay_rate = decay_rate
         self.label_smoothing = label_smoothing
         self.cuda = cuda
-        self.kwargs = {"input_dropout": input_dropout, "hidden_dropout": hidden_dropout,
-                       "feature_map_dropout": feature_map_dropout, "in_channels":in_channels,
-                       "out_channels": out_channels, "filt_h": filt_h, "filt_w": filt_w}
+        self.kwargs = {"input_dropout": input_dropout,
+                       "hidden_dropout": hidden_dropout,
+                       "feature_map_dropout": feature_map_dropout,
+                       "in_channels":in_channels,
+                       "out_channels": out_channels,
+                       "filt_h": filt_h,
+                       "filt_w": filt_w}
         
     def get_data_idxs(self, data):
-        data_idxs = [(self.entity_idxs[data[i][0]], self.relation_idxs[data[i][1]], \
+        data_idxs = [(self.entity_idxs[data[i][0]],
+                      self.relation_idxs[data[i][1]],
                       self.entity_idxs[data[i][2]]) for i in range(len(data))]
+
         return data_idxs
     
     def get_er_vocab(self, data):
         er_vocab = defaultdict(list)
+
         for triple in data:
             er_vocab[(triple[0], triple[1])].append(triple[2])
+
         return er_vocab
 
     def get_batch(self, er_vocab, er_vocab_pairs, idx):
         batch = er_vocab_pairs[idx:min(idx+self.batch_size, len(er_vocab_pairs))]
         targets = np.zeros((len(batch), len(d.entities)))
+
         for idx, pair in enumerate(batch):
             targets[idx, er_vocab[pair]] = 1.
+
         targets = torch.FloatTensor(targets)
+
         if self.cuda:
             targets = targets.cuda()
+
         return np.array(batch), targets
 
-    
     def evaluate(self, model, data):
         hits = []
         ranks = []
+
         for i in range(10):
             hits.append([])
 
@@ -68,10 +99,12 @@ class Experiment:
             e1_idx = torch.tensor(data_batch[:,0])
             r_idx = torch.tensor(data_batch[:,1])
             e2_idx = torch.tensor(data_batch[:,2])
+
             if self.cuda:
                 e1_idx = e1_idx.cuda()
                 r_idx = r_idx.cuda()
                 e2_idx = e2_idx.cuda()
+
             predictions = model.forward(e1_idx, r_idx)
 
             for j in range(data_batch.shape[0]):
@@ -82,10 +115,9 @@ class Experiment:
 
             sort_values, sort_idxs = torch.sort(predictions, dim=1, descending=True)
 
-            sort_idxs = sort_idxs.cpu().numpy()
             for j in range(data_batch.shape[0]):
-                rank = np.where(sort_idxs[j]==e2_idx[j])[0][0]
-                ranks.append(rank+1)
+                rank = np.where(sort_idxs[j] == e2_idx[j])[0][0]
+                ranks.append(rank + 1)
 
                 for hits_level in range(10):
                     if rank <= hits_level:
@@ -98,9 +130,6 @@ class Experiment:
         print('Hits @1: {0}'.format(np.mean(hits[0])))
         print('Mean rank: {0}'.format(np.mean(ranks)))
         print('Mean reciprocal rank: {0}'.format(np.mean(1./np.array(ranks))))
-
-
-
 
     def train_and_eval(self):
         print("Training the %s model..." % model_name)
@@ -134,24 +163,30 @@ class Experiment:
 
         print("Starting training...")
 
-        for it in range(1, self.num_iterations+1):
+        for it in range(1, self.num_iterations + 1):
             model.train()    
             losses = []
             np.random.shuffle(er_vocab_pairs)
+
             for j in range(0, len(er_vocab_pairs), self.batch_size):
                 data_batch, targets = self.get_batch(er_vocab, er_vocab_pairs, j)
                 opt.zero_grad()
-                e1_idx = torch.tensor(data_batch[:,0])
-                r_idx = torch.tensor(data_batch[:,1])
+                e1_idx = torch.tensor(data_batch[:, 0])
+                r_idx = torch.tensor(data_batch[:, 1])
+
                 if self.cuda:
                     e1_idx = e1_idx.cuda()
                     r_idx = r_idx.cuda()
+
                 predictions = model.forward(e1_idx, r_idx)
+
                 if self.label_smoothing:
                     targets = ((1.0-self.label_smoothing)*targets) + (1.0/targets.size(1))
+
                 loss = model.loss(predictions, targets)
                 loss.backward()
                 opt.step()
+
             if self.decay_rate:
                 scheduler.step()
             losses.append(loss.item())
@@ -163,32 +198,52 @@ class Experiment:
             with torch.no_grad():
                 print("Validation:")
                 self.evaluate(model, d.valid_data)
-                if not it%2:
+                if not it % 2:
                     print("Test:")
                     self.evaluate(model, d.test_data)
 
 
-        
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--algorithm', type=str, default="HypER", nargs="?",
-                    help='Which algorithm to use: HypER, ConvE, DistMult, or ComplEx')
-    parser.add_argument('--dataset', type=str, default="FB15k-237", nargs="?",
-                    help='Which dataset to use: FB15k, FB15k-237, WN18 or WN18RR')
+    parser.add_argument('--algorithm',
+                        type=str,
+                        default="HypER",
+                        nargs="?",
+                        help='Which algorithm to use: HypER, ConvE, DistMult, or ComplEx')
+    parser.add_argument('--dataset',
+                        type=str,
+                        default="FB15k-237",
+                        nargs="?",
+                        help='Which dataset to use: FB15k, FB15k-237, WN18 or WN18RR')
+
     args = parser.parse_args()
     model_name = args.algorithm
     dataset = args.dataset
+
     data_dir = "data/%s/" % dataset
-    torch.backends.cudnn.deterministic = True 
+    d = Data(data_dir=data_dir, reverse=True)
+
+    torch.backends.cudnn.deterministic = True
     seed = 42
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available:
-        torch.cuda.manual_seed_all(seed)  
-    d = Data(data_dir=data_dir, reverse=True)
-    experiment = Experiment(model_name, num_iterations=800, batch_size=128, learning_rate=0.001, 
-                            decay_rate=0.99, ent_vec_dim=200, rel_vec_dim=200, cuda=True, 
-                            input_dropout=0.2, hidden_dropout=0.3, feature_map_dropout=0.2,
-                            in_channels=1, out_channels=32, filt_h=1, filt_w=9, label_smoothing=0.1)
+        torch.cuda.manual_seed_all(seed)
+
+    experiment = Experiment(model_name,
+                            num_iterations=800,
+                            batch_size=128,
+                            learning_rate=0.001,
+                            decay_rate=0.99,
+                            ent_vec_dim=200,
+                            rel_vec_dim=200,
+                            cuda=False,
+                            input_dropout=0.2,
+                            hidden_dropout=0.3,
+                            feature_map_dropout=0.2,
+                            in_channels=1,
+                            out_channels=32,
+                            filt_h=1,
+                            filt_w=9,
+                            label_smoothing=0.1)
     experiment.train_and_eval()
