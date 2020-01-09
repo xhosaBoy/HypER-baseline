186c3d3e39d5e963af26b5a0bc55042c4fb6cd09
Chief <luyolo.nqobile@gmail.com>
2020-01-09 13:31:55 +0200

    HypER Baseline FB15k Results
    Align number of cost evaluation data points.

--- HypER/hyper.py
+++ HypER/hyper.py
@@ -264,7 +264,7 @@ class Experiment:
             model.eval()
             with torch.no_grad():
                 train_data = np.array(d.train_data)
-                train_data = train_data[np.random.choice(train_data.shape[0], 10000, replace=False), :]
+                train_data = train_data[np.random.choice(train_data.shape[0], 118142, replace=False), :]
                 self.evaluate(model, train_data, epoch, 'training')
                 logger.info(f'Starting Validation ...')
                 self.evaluate(model, d.valid_data, epoch, 'validation')
