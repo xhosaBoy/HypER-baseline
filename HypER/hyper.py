ed4c140020746bde815e2124b8a5cb3c5c333b95
xhosaBoy <luyolo.nqobile@gmail.com>
2020-07-13 21:07:23 +0200

    Pre-Trained Word Vector Experiment
    Train using FB15k-237 KG.

--- HypER/hyper.py
+++ HypER/hyper.py
@@ -23,7 +23,7 @@ stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
-file_handler = logging.FileHandler('hntn_train_validate_and_test_wn18rr_200d_baseline.log')
+file_handler = logging.FileHandler('hntn_train_validate_and_test_fb15k_237_200d_baseline.log')
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -300,7 +300,7 @@ if __name__ == '__main__':
                         help='Which algorithm to use: HypER, ConvE, DistMult, or ComplEx')
     parser.add_argument('--dataset',
                         type=str,
-                        default="WN18RR",
+                        default="FB15k-237",
                         nargs="?",
                         help='Which dataset to use: FB15k, FB15k-237, WN18 or WN18RR')
 
